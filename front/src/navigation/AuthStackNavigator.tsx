c83846cb0d6a0b9aa3f204dbf5d66b4d21de1bac
권형석 <hskwon517@gmail.com>
2024-04-02 21:10:29 +0900

    temp

--- front/src/navigation/AuthStackNavigator.tsx
+++ front/src/navigation/AuthStackNavigator.tsx
@@ -1,15 +1,24 @@
 import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
 import {StyleSheet} from 'react-native';
+import {authNavigations} from '../constants';
 import AuthHomeScreen from '../screens/AuthHomeScreen';
 import LoginScreen from '../screens/LoginScreen';
 
+export type AuthStackParamList = {
+  [authNavigations.AUTH_HOME]: undefined;
+  [authNavigations.LOGIN]: undefined;
+};
+
 function AuthStackNavigator() {
-  const Stack = createStackNavigator();
+  const Stack = createStackNavigator<AuthStackParamList>();
   return (
     <Stack.Navigator>
-      <Stack.Screen name="AuthHome" component={AuthHomeScreen} />
-      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen
+        name={authNavigations.AUTH_HOME}
+        component={AuthHomeScreen}
+      />
+      <Stack.Screen name={authNavigations.LOGIN} component={LoginScreen} />
     </Stack.Navigator>
   );
 }
